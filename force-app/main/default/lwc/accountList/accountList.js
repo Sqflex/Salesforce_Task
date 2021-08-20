b98684f194d7e961f1be07f2181847aa37ce728a
SQflex <egokazgorki2@gmail.com>
2021-08-20 16:37:57 +0300

    fix: accountCreate error

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,14 +1,11 @@
 import { LightningElement, track, wire } from 'lwc';
-import {deleteRecord} from 'lightning/uiRecordApi';
-import {refreshApex} from '@salesforce/apex';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import FIRST_NAME_FIELD from '@salesforce/schema/DataObj__c.FirstName__c';
 import RECORD_DATE_FIELD from '@salesforce/schema/DataObj__c.RecordDate__c';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Recorded Date', fieldName: RECORD_DATE_FIELD.fieldApiName, type: 'date' },
-    { label: "Delete Record", type:"button", onclick: '{handleDeleteAction}', typeAttributes: {
+    { label: "Delete Record", type:"button", typeAttributes: {
         iconName: 'utility:delete',
         label: 'Delete Record',
         name: 'Delete Record',
@@ -19,22 +16,4 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getAccounts)
     accounts;
-    @track recordId;
-
-    handleDeleteAction(event){
-        this.recordId = event.target.value;
-        console.log(this.recordId);
-        
-        deleteRecord(this.recordId).then(() =>{
- 
-            const toastEvent = new ShowToastEvent({
-                title:'Record Deleted',
-                message:'Record deleted successfully',
-                variant:'success',
-            })
-            this.dispatchEvent(toastEvent);
-     
-            return refreshApex(this.accounts);
-        })
-    }
 }
